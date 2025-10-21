b4fab4b72546637f044e4dabcc031c7b76549aa6
Mamunuri Sai Sreemanth <147626351+sreemanth2909@users.noreply.github.com>
2025-10-21 14:28:21 +0530

    Implement hierarchical scope tracking in symbol table

--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -9,6 +9,8 @@ using namespace std;
 Symbol symtab[MAX_SYMBOLS];
 int symCount = 0;
 int current_scope = 0;
+int parent_scopes[100];  // Stack to track parent scope relationships
+int scope_depth = 0;     // Current depth in scope stack
 int current_offset = 0;
 char currentType[128] = "int";
 
@@ -40,6 +42,7 @@ void insertVariable(const char* name, const char* type, int is_array, int* dims,
     strcpy(symtab[symCount].type, type);
     strcpy(symtab[symCount].kind, is_array ? "array" : (ptr_level > 0 ? "pointer" : "variable"));
     symtab[symCount].scope_level = current_scope;
+    symtab[symCount].parent_scope = (scope_depth > 0) ? parent_scopes[scope_depth - 1] : -1;
     symtab[symCount].offset = current_offset;
     symtab[symCount].is_array = is_array;
     symtab[symCount].ptr_level = ptr_level;
@@ -71,7 +74,8 @@ void insertFunction(const char* name, const char* ret_type, int param_count, cha
     strcpy(symtab[symCount].name, name);
     strcpy(symtab[symCount].return_type, ret_type);
     strcpy(symtab[symCount].kind, "function");
-    symtab[symCount].scope_level = 0;
+    symtab[symCount].scope_level = 0;  // Functions are always at global scope
+    symtab[symCount].parent_scope = -1; // No parent for global scope
     symtab[symCount].is_function = 1;
     symtab[symCount].param_count = param_count;
     
@@ -86,23 +90,64 @@ void insertFunction(const char* name, const char* ret_type, int param_count, cha
 }
 
 Symbol* lookupSymbol(const char* name) {
+    // Search from innermost scope outward to global scope
     for (int i = symCount - 1; i >= 0; i--) {
-        if (strcmp(symtab[i].name, name) == 0 && symtab[i].scope_level <= current_scope) {
-            return &symtab[i];
+        if (strcmp(symtab[i].name, name) == 0) {
+            // Check if this symbol is in current scope or any parent scope
+            int check_scope = current_scope;
+            while (check_scope >= 0) {
+                if (symtab[i].scope_level == check_scope) {
+                    return &symtab[i];
+                }
+                // Move to parent scope
+                if (check_scope == 0) break;
+                // Find parent scope by looking up the scope stack
+                bool found_parent = false;
+                for (int j = scope_depth - 1; j >= 0; j--) {
+                    if (parent_scopes[j] < check_scope) {
+                        check_scope = parent_scopes[j];
+                        found_parent = true;
+                        break;
+                    }
+                }
+                if (!found_parent) check_scope = 0; // Go to global
+            }
         }
     }
     return NULL;
 }
 
 void enterScope() {
+    // Push current scope onto parent stack
+    if (scope_depth < 100) {
+        parent_scopes[scope_depth] = current_scope;
+        scope_depth++;
+    }
     current_scope++;
 }
 
 void exitScope() {
-    while (symCount > 0 && symtab[symCount - 1].scope_level == current_scope) {
-        symCount--;
+    // Don't remove symbols - keep them for symbol table display
+    // Just update the current scope level
+    if (scope_depth > 0) {
+        scope_depth--;
+        current_scope = parent_scopes[scope_depth];
+    } else {
+        current_scope = 0;
     }
-    current_scope--;
+}
+
+void moveRecentSymbolsToCurrentScope(int count) {
+    // Move the last 'count' non-function symbols to the current scope
+    // This is used to move function parameters from global scope to function scope
+    int moved = 0;
+    for (int i = symCount - 1; i >= 0 && moved < count; i--) {
+        if (!symtab[i].is_function && symtab[i].scope_level == 0) {
+            symtab[i].scope_level = current_scope;
+            symtab[i].parent_scope = (scope_depth > 0) ? parent_scopes[scope_depth - 1] : -1;
+            moved++;
+        }
+    }
 }
 
 void insertSymbol(const char* name, const char* type, int is_function) {
@@ -154,20 +199,61 @@ void setCurrentType(const char* type) {
 }
 
 void printSymbolTable() {
-    cout << "\n=== SYMBOL TABLE ===" << endl;
+    cout << "\n=== SYMBOL TABLE (Hierarchical Scope View) ===" << endl;
     cout << left << setw(20) << "Name"
          << setw(15) << "Type"
          << setw(10) << "Kind"
          << setw(10) << "Scope"
+         << setw(12) << "Parent"
          << setw(10) << "Size" << endl;
-    cout << "------------------------------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    
+    // Find the maximum scope level in the symbol table
+    int max_scope = 0;
     for (int i = 0; i < symCount; i++) {
-        cout << left << setw(20) << symtab[i].name
-             << setw(15) << (symtab[i].is_function ? symtab[i].return_type : symtab[i].type)
-             << setw(10) << symtab[i].kind
-             << setw(10) << symtab[i].scope_level
-             << setw(10) << symtab[i].size << endl;
+        if (symtab[i].scope_level > max_scope) {
+            max_scope = symtab[i].scope_level;
+        }
     }
-    cout << "------------------------------------------------------------------------" << endl;
-    cout << "Total symbols: " << symCount << endl << endl;
+    
+    // Group by scope level for better visualization
+    for (int scope = 0; scope <= max_scope; scope++) {
+        bool has_symbols = false;
+        for (int i = 0; i < symCount; i++) {
+            if (symtab[i].scope_level == scope) {
+                has_symbols = true;
+                break;
+            }
+        }
+        
+        if (has_symbols) {
+            if (scope == 0) {
+                cout << ">>> GLOBAL SCOPE (0) <<<" << endl;
+            } else {
+                cout << ">>> SCOPE LEVEL " << scope << " <<<" << endl;
+            }
+            
+            for (int i = 0; i < symCount; i++) {
+                if (symtab[i].scope_level == scope) {
+                    // Add indentation based on scope depth
+                    string indent(scope * 2, ' ');
+                    cout << indent << left << setw(20 - scope * 2) << symtab[i].name
+                         << setw(15) << (symtab[i].is_function ? symtab[i].return_type : symtab[i].type)
+                         << setw(10) << symtab[i].kind
+                         << setw(10) << symtab[i].scope_level;
+                    
+                    if (symtab[i].parent_scope == -1) {
+                        cout << setw(12) << "none";
+                    } else {
+                        cout << setw(12) << symtab[i].parent_scope;
+                    }
+                    
+                    cout << setw(10) << symtab[i].size << endl;
+                }
+            }
+        }
+    }
+    
+    cout << "--------------------------------------------------------------------------------" << endl;
+    cout << "Total symbols: " << symCount << " | Max scope level: " << max_scope << endl << endl;
 }
